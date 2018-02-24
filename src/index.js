93c2382013dbf56d97936b80aeabce7f673df470
vlukyaneRubyLabs <vlukyane@gmail.com>
2018-02-24 09:00:10 +0300

    Fifth task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    }
+    var H = 0, Q = 0, D = 0, N = 0, P = 0, ans = {};
+    if (currency >= 50){
+        H = Math.floor(currency / 50);
+        currency -= H * 50;
+        ans['H'] = H;
+    }
+    if (currency >= 25){
+        Q = Math.floor(currency / 25);
+        currency -= Q * 25;
+        ans['Q'] = Q;
+    }
+    if (currency >= 10){
+        D = Math.floor(currency / 10);
+        currency -= D * 10;
+        ans['D'] = D;
+    }
+    if (currency >= 5){
+        N = Math.floor(currency / 5);
+        currency -= N * 5;
+        ans['N'] = N;
+    }
+    if (currency >= 1){
+        P = Math.floor(currency);
+        currency = 0;
+        ans['P'] = P;
+    }
+
+    return ans;
+};
